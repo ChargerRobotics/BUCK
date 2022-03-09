bdd96c554120735e798cc2331259abac561c9e29
Dallin24 <the4gates24@gmail.com>
2022-03-09 12:23:15 -0700

    Updates to 2022 Commands, Removed Nonfunctioning Code
    Also added method to test cheesy drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,47 +1,45 @@
 package frc.robot;
 
-import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot 
 {
+
   /* DRIVETRAIN */
   /***********************************************************************************************************************************************/
-
   // INSTANTIATE LEFT MOTORS AND LEFT DRIVE
-  public SpeedController leftBackDrive = new PWMVictorSPX(2);
-  public SpeedController leftFrontDrive = new PWMVictorSPX(1);
-  public SpeedControllerGroup leftDrive = new SpeedControllerGroup(leftBackDrive, leftFrontDrive);
+  public MotorController leftBackDrive = new PWMVictorSPX(4);
+  public MotorController leftFrontDrive = new PWMVictorSPX(5);
+  public MotorControllerGroup leftDrive = new MotorControllerGroup(leftBackDrive, leftFrontDrive);
 
   // INSTANTIATE RIGHT MOTORS AND RIGHT DRIVE
-  public SpeedController rightBackDrive = new PWMVictorSPX(4);
-  public SpeedController rightFrontDrive = new PWMVictorSPX(3);
-  public SpeedControllerGroup rightDrive = new SpeedControllerGroup(rightBackDrive, rightFrontDrive);
-
+  public MotorController rightBackDrive = new PWMVictorSPX(3);
+  public MotorController rightFrontDrive = new PWMVictorSPX(2);
+  public MotorControllerGroup rightDrive = new MotorControllerGroup(rightBackDrive, rightFrontDrive);
   /***********************************************************************************************************************************************/
   
-  /* FLYWHEEL AND STORAGE */
+  /* STORAGE AND FLYWHEEL*/
   /***********************************************************************************************************************************************/
+  // INSTANTIATE STORAGE MOTOR
+  public PWMVictorSPX storageMotor = new PWMVictorSPX(9);
+  //public Spark storageMotor = new Spark(9);
 
   // INSTANTIATE FLYWHEEL MOTORS AND FLYWHEEL
-  public SpeedController leftFlywheelMotor = new PWMVictorSPX(5);
-  public SpeedController rightFlywheelMotor = new PWMVictorSPX(6);
-
-  // INSTANTIATE STORAGE MOTORS
-  public SpeedController storageMotor = new PWMVictorSPX(7);
-
+  public MotorController leftFlywheelMotor = new PWMVictorSPX(7);
+  public MotorController rightFlywheelMotor = new PWMVictorSPX(8);
   /***********************************************************************************************************************************************/ 
 
-  /* CONTROLLER AND CAMERA */
+  /* CONTROLLER */
   /***********************************************************************************************************************************************/
-
   // INSTANTIATE JOYSTICKS
   public Joystick leftJoystick = new Joystick(0);
   public Joystick rightJoystick = new Joystick(1);
@@ -56,25 +54,31 @@ public class Robot extends TimedRobot
    public static int leftSlider = 2;
 
   // INSTANTIATE JOYSTICK BUTTONS
+  public JoystickButton l1Button = new JoystickButton(leftJoystick, 1);
+  public JoystickButton l2Button = new JoystickButton(leftJoystick, 2);
+  public JoystickButton l3Button = new JoystickButton(leftJoystick, 3);
+  public JoystickButton l4Button = new JoystickButton(leftJoystick, 4);
+  public JoystickButton l5Button = new JoystickButton(leftJoystick, 5);
+  public JoystickButton l6Button = new JoystickButton(leftJoystick, 6);
+   
+  // INSTANTIATE JOYSTICK BUTTONS
   public JoystickButton r1Button = new JoystickButton(rightJoystick, 1);
   public JoystickButton r2Button = new JoystickButton(rightJoystick, 2);
   public JoystickButton r3Button = new JoystickButton(rightJoystick, 3);
   public JoystickButton r4Button = new JoystickButton(rightJoystick, 4);
   public JoystickButton r5Button = new JoystickButton(rightJoystick, 5);
-
-
+  public JoystickButton r6Button = new JoystickButton(rightJoystick, 6);
   /***********************************************************************************************************************************************/
-  
+
   /** This function is called once whenever the robot starts a sequence. */
   @Override
   public void robotInit() 
   {
-    leftFlywheelMotor.setVoltage(0);
-    rightFlywheelMotor.setVoltage(0);
-
-    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+    // START CAMERA
+    UsbCamera camera = CameraServer.startAutomaticCapture();
     camera.setFPS(30);
     camera.setResolution(640, 480);
+      
   }
 
   /** This function is called periodically when the robot is in a sequence. */
@@ -87,12 +91,13 @@ public class Robot extends TimedRobot
 
   /** This function is called periodically when the robot is in auto. */
   @Override
-  public void autonomousPeriodic(){}
+  public void autonomousPeriodic() {}
 
   /** This function is called once when the robot starts teleop. */
   @Override
   public void teleopInit() 
   {
+    //NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
     leftFlywheelMotor.setVoltage(0);
     rightFlywheelMotor.setVoltage(0);
     storageMotor.setVoltage(0);
@@ -104,74 +109,78 @@ public class Robot extends TimedRobot
   {
     /* CONTROLLER */
     /***********************************************************************************************************************************************/
-    
     // MAPPING BUTTONS AND JOYSTICK INPUTS TO VARIABLES
-    double leftY = leftJoystick.getRawAxis(leftStickY);
-    double rightY = rightJoystick.getRawAxis(rightStickY);
+    double leftSliderValue = leftJoystick.getRawAxis(leftSlider);
+    leftSliderValue *= -1;
+    System.out.println(leftSliderValue);
+    if (leftSlider < 0)
+    {
+      leftSliderValue = 0.5;
+    }
 
     boolean right1 = r1Button.get();
     boolean right2 = r2Button.get();
-    boolean right3 = r3Button.get();
-    boolean right4 = r4Button.get();
     boolean right5 = r5Button.get();
-
     /***********************************************************************************************************************************************/
     
     /* DRIVETRAIN */
     /***********************************************************************************************************************************************/
-    
-    // SCALE DOWN JOYSTICK AXIS VALUES
-    leftY = leftY*.8;
-    rightY = rightY*.8;
+    // // SCALE DOWN JOYSTICK AXIS VALUES
+    // leftY = leftY*.8;
+    // rightY = rightY*.8;
 
-    // READS BUTTON VALUES TO ADJUST SPEED OF DRIVETRAIN
-    if (right3 == true) 
-    {
-      leftY = leftY * .95;
-      rightY = rightY * .95;
-    } 
-    else 
-    {
-      leftY = leftY * .75;
-      rightY = rightY * .75;
-    }
+    // // READS BUTTON VALUES TO ADJUST SPEED OF DRIVETRAIN
+    // if (right3 == true) 
+    // {
+    //   leftY = leftY * .95;
+    //   rightY = rightY * .95;
+    // } 
+    // else 
+    // {
+    //   leftY = leftY * .75;
+    //   rightY = rightY * .75;
+    // }
 
-    // REMOVES CONTROLLER DRIFT
-    if (leftY < .1) 
-    {
-      if (leftY > -.1) 
-      {
-        leftY = 0;
-      }
-    }
-    if (rightY < .1) 
-    {
-      if (rightY > -.1) 
-      {
-        rightY = 0;
-      }
-    }
+    // // REMOVES CONTROLLER DRIFT
+    // if (leftY < .1) 
+    // {
+    //   if (leftY > -.1) 
+    //   {
+    //     leftY = 0;
+    //   }
+    // }
+    // if (rightY < .1) 
+    // {
+    //   if (rightY > -.1) 
+    //   {
+    //     rightY = 0;
+    //   }
+    // }
 
-    // SETS DRIVE SIDES TO AXES
-    leftDrive.set(-leftY);
-    rightDrive.set(rightY);
-
+    // // SETS DRIVE SIDES TO AXES
+    // leftDrive.set(-leftY);
+    // rightDrive.set(rightY);
     /***********************************************************************************************************************************************/
     
-
-    /* FLYWHEEL w/STORAGE */
+    /* FLYWHEEL AND STORAGE */
     /***********************************************************************************************************************************************/
-    
     // IF RIGHT TRIGGER PRESSED, ACTIVATE FLYWHEEL MOTORS
     if(right1 == true) 
     {
-      leftFlywheelMotor.setVoltage(-12);
-      rightFlywheelMotor.setVoltage(-11);
+      leftFlywheelMotor.setVoltage(-12 * leftSliderValue);
+      rightFlywheelMotor.setVoltage(-11 * leftSliderValue);
     }
     else
     {
       leftFlywheelMotor.set(0);
       rightFlywheelMotor.set(0);
+    }
+
+    if(right5 == true)
+    {
+      leftFlywheelMotor.setVoltage(8);
+      rightFlywheelMotor.setVoltage(8);
+      storageMotor.setVoltage(-10);
     }
 
     // IF BUTTON PRESSED, ACTIVATE STORAGE MOTOR
@@ -182,15 +191,6 @@ public class Robot extends TimedRobot
     {
       storageMotor.set(0);
     }
-
-    // IF BUTTON PRESSED, REVERSE FLYWHEEL AND STORAGE
-    if(right5 == true)
-    {
-      leftFlywheelMotor.setVoltage(8);
-      rightFlywheelMotor.setVoltage(8);
-      storageMotor.setVoltage(-10);
-    }
-
     /***********************************************************************************************************************************************/
   }
 
